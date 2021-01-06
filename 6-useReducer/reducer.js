e508f43109fe6f8fc81b40eb1c216ae2092f2f66
Vijay Jha <51393417+vjha23@users.noreply.github.com>
2021-01-06 20:41:57 +0530

    Add files via upload

---
+++ 6-useReducer/reducer.js
@@ -0,0 +1,26 @@
+import { act } from "react-dom/test-utils";
+
+export const reducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const newPeople = [...state.people, action.payload];
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'item added',
+        };
+    }
+    if (action.type === 'NO_VALUE') {
+        return { ...state, isModalOpen: true, modalContent: 'please enter value' }
+    }
+    if (action.type === 'CLOSE_MODAL') {
+        return { ...state, isModalOpen: false }
+    }
+    if (action.type === 'REMOVE_ITEM') {
+        const newPeople = state.people.filter(
+            (person) => person.id !== action.payload
+        );
+        return { ...state, people: newPeople };
+    }
+    throw new Error('no matching action typee');
+}
